3b55351b5a8056cf05a7b10b79f0b06042198a36
Summer Chen <mp4bj6ruby@gmail.com>
2020-10-28 23:14:31 +0800

    Added react-helmet

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,10 +5,15 @@ import GlobalStyle from '@/utils/global-style';
 import { rootRoutes } from '@/routes';
 import 'antd/dist/antd.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Helmet } from 'react-helmet';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>MS反波膽</title>
+      </Helmet>
       <Switch>
         {rootRoutes.map((route, i) => (
           <RouteWithSubRoutes key={i} {...route} />
