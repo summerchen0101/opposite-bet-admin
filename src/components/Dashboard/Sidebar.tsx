efbb889010045dace3e11b3e5573dde3c1d75e6a
Summer Chen <mp4bj6ruby@gmail.com>
2020-11-05 01:07:39 +0800

    logo added home link

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -1,20 +1,33 @@
 import React, { useContext } from 'react'
 import { Layout, Menu } from 'antd'
-import { Link, useLocation, useRouteMatch } from 'react-router-dom'
+import { Link, useHistory, useLocation, useRouteMatch } from 'react-router-dom'
 import DashboardContext from '@/contexts/DashboardContext'
 import MenuWithSubMenu from '@/utils/MenuWithSubMenu'
 import { menu } from '@/routes'
+import styled from 'styled-components'
 
 const { Sider } = Layout
+
+const LogoWrapper = styled.div`
+  height: 32px;
+  font-size: 20px;
+  color: #fff;
+  margin: 16px;
+  text-align: center;
+  cursor: pointer;
+`
 
 const Sidebar: React.FC = () => {
   const { collapsed } = useContext(DashboardContext)
   const { pathname } = useLocation()
   const openKeys = ['/' + pathname.split('/')[1]]
   const selectKey = [pathname]
+  const history = useHistory()
   return (
     <Sider trigger={null} collapsible collapsed={collapsed}>
-      <div className="logo">{!collapsed ? '新體育後台' : '體'}</div>
+      <LogoWrapper onClick={() => history.push('/')}>
+        {!collapsed ? '新體育後台' : '體'}
+      </LogoWrapper>
       <Menu
         theme="dark"
         mode="inline"
