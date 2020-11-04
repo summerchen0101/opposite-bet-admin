36c1ace92f534e5a70dda02261bdcbce1c11b486
Summer Chen <mp4bj6ruby@gmail.com>
2020-11-04 21:20:29 +0800

    component Table: style modify

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,12 +1,13 @@
 import { Table as AntTable } from 'antd'
 import { ColumnType } from 'antd/lib/table'
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 interface IProps {
   data: any[]
   columns: ColumnType<any>[]
 }
-const Table: React.FC<IProps> = ({ data, columns }) => {
+const Table: React.FC<IProps> = ({ data, columns, ...props }) => {
   const [columnsWithKey, setColumnsWithKey] = useState([])
   useEffect(() => {
     setColumnsWithKey(
@@ -24,8 +25,16 @@ const Table: React.FC<IProps> = ({ data, columns }) => {
       scroll={{ x: 1000 }}
       sticky={{ offsetHeader: -24 }}
       pagination={{ pageSize: 30 }}
+      {...props}
     />
   )
 }
 
-export default Table
+export default styled(Table)`
+  .ant-table-container {
+    margin: 0 -25px;
+  }
+  .ant-table-sticky-header {
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+  }
+`
