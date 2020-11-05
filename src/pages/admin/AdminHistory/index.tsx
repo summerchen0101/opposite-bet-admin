8eb7ffb48f763747366849f1381f7ddabd366d8f
Summer Chen <mp4bj6ruby@gmail.com>
2020-11-06 00:31:38 +0800

    AdminHistory: search components

--- src/pages/admin/AdminHistory/index.tsx
+++ src/pages/admin/AdminHistory/index.tsx
@@ -1,13 +1,9 @@
 import Dashboard from '@/components/Dashboard'
 import DateRangePicker from '@/components/DateRangePicker'
 import PageSearchBar from '@/components/PageSearchBar'
-import SearchInput from '@/components/SearchInput'
-import MultipleSelector from './containers/MultipleSelector'
-import RelativeDateBtns from '@/components/RelativeDateBtns'
 import { useReducerInjector } from '@/utils/hooks'
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import CreateButton from './containers/CreateButton'
 import PageHeader from './components/PageHeader'
 import PopupCreateForm from './containers/PopupCreateForm'
 import TableData from './containers/TableData'
@@ -22,12 +18,8 @@ const Manager: React.FC = () => {
   return (
     <Dashboard>
       <PageHeader />
-      <PageSearchBar style={{ marginBottom: 10 }}>
-        <MultipleSelector />
-      </PageSearchBar>
       <PageSearchBar>
         <DateRangePicker />
-        <RelativeDateBtns />
       </PageSearchBar>
       <TableData />
       <PopupCreateForm />
