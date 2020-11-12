04fc970d1cee4e7f6eb93f5d2849f62e5aa5e79c
Summer Chen <mp4bj6ruby@gmail.com>
2020-11-12 12:34:03 +0800

    WithdrawalSetting

--- src/pages/payment/WithdrawalSetting/containers/PopupCreateForm.tsx
+++ src/pages/payment/WithdrawalSetting/containers/PopupCreateForm.tsx
@@ -1,10 +1,11 @@
+import { FormStaticText } from '@/components'
+import Form, { FormField } from '@/components/Form'
 import PopupModal from '@/components/PopupModal'
-import { Button, DatePicker, Input, Select, Space, Form as AntForm } from 'antd'
+import { Button, Col, Input, Row, Select, Space, Table } from 'antd'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { toggleCreateModal } from '../reducer'
 import { selectDisplayCreateModal, useTypedSelector } from '../selectors'
-import Form, { FormField } from '@/components/Form'
 const { Option } = Select
 const CreateForm: React.FC = () => {
   const dispatch = useDispatch()
@@ -20,31 +21,76 @@ const CreateForm: React.FC = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
   }
+  const layout = {
+    labelCol: { span: 8 },
+  }
+  let columns = [
+    {
+      title: '上限',
+      render: () => <Input itemType="number" placeholder="5000" />,
+    },
+    {
+      title: '下限',
+      render: () => <Input itemType="number" placeholder="0" />,
+    },
+  ]
+  columns = columns.map((t, i) => ({ ...t, key: i }))
+  const data = [...Array(1)].map((t, i) => ({ ...t, key: i }))
   return (
-    <PopupModal visible={isDisplay} title="新增賽事" onCancel={onCancel}>
-      <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <FormField label="請選擇隊伍(主)" name="mainTeam" required>
-          <Input />
-        </FormField>
-        <FormField label="請選擇隊伍" name="clientTeam" required>
-          <Input />
-        </FormField>
-        <FormField label="國家" name="country" required>
-          <Select placeholder="請選擇" allowClear>
-            <Option value="opt1">巴西</Option>
-            <Option value="opt2">美國</Option>
-          </Select>
-        </FormField>
-        <FormField label="請選擇聯盟" name="league" required>
-          <Input />
-        </FormField>
-        <FormField label="開賽時間" name="startAt" required>
-          <DatePicker style={{ width: '100%' }} />
-        </FormField>
-        <FormField>
-          <p>＊如果上面結果為撤銷則不用選擇</p>
-        </FormField>
-
+    <PopupModal
+      visible={isDisplay}
+      title="新增存款資訊"
+      onCancel={onCancel}
+      width={800}
+    >
+      <Form
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        layout="horizontal"
+        labelAlign="left"
+        {...layout}
+      >
+        <Row gutter={40}>
+          <Col span={12}>
+            <FormField label="名稱">
+              <Input />
+            </FormField>
+            <FormField label="銀行名稱">
+              <Select placeholder="請選擇" allowClear defaultValue="opt1">
+                <Option value="opt1">822 中國信託</Option>
+              </Select>
+            </FormField>
+            <FormField label="控端提示">
+              <Select placeholder="請選擇" allowClear defaultValue="opt1">
+                <Option value="opt1">存入</Option>
+                <Option value="opt2">提出</Option>
+              </Select>
+            </FormField>
+            <div style={{ marginTop: 30 }}></div>
+            <h3>入款限額設置</h3>
+            <FormField>
+              <Table
+                bordered
+                size="small"
+                columns={columns}
+                dataSource={data}
+                pagination={false}
+              />
+            </FormField>
+          </Col>
+          <Col span={12}>
+            <h3>會員端顯示</h3>
+            <FormField label="收款人" className="narrow">
+              <FormStaticText>王大明</FormStaticText>
+            </FormField>
+            <FormField label="開戶行網點" className="narrow">
+              <FormStaticText>11239</FormStaticText>
+            </FormField>
+            <FormField label="帳號" className="narrow">
+              <FormStaticText>11234567890</FormStaticText>
+            </FormField>
+          </Col>
+        </Row>
         <FormField style={{ marginTop: '20px', textAlign: 'center' }}>
           <Space size="large">
             <Button type="primary" htmlType="submit">
