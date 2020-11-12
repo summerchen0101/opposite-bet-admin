6a7525cc014b68fa75b2f827ff7d586d240372e9
Summer Chen <mp4bj6ruby@gmail.com>
2020-11-12 15:33:31 +0800

    PromoteLink: popup

--- src/pages/promote/PromoteLink/containers/PopupCreateForm.tsx
+++ src/pages/promote/PromoteLink/containers/PopupCreateForm.tsx
@@ -1,10 +1,19 @@
 import PopupModal from '@/components/PopupModal'
-import { Button, DatePicker, Input, Select, Space, Form as AntForm } from 'antd'
+import {
+  Button,
+  DatePicker,
+  Input,
+  Select,
+  Space,
+  Form as AntForm,
+  Alert,
+} from 'antd'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { toggleCreateModal } from '../reducer'
 import { selectDisplayCreateModal, useTypedSelector } from '../selectors'
 import Form, { FormField } from '@/components/Form'
+import { CascaderSelector } from '@/components'
 const { Option } = Select
 const CreateForm: React.FC = () => {
   const dispatch = useDispatch()
@@ -20,29 +29,73 @@ const CreateForm: React.FC = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
   }
+  const options = [
+    {
+      value: 'zhejiang',
+      label: 'Zhejiang',
+      children: [
+        {
+          value: 'hangzhou',
+          label: 'Hangzhou',
+          children: [
+            {
+              value: 'xihu',
+              label: 'West Lake',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu',
+      children: [
+        {
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [
+            {
+              value: 'zhonghuamen',
+              label: 'Zhong Hua Men',
+            },
+          ],
+        },
+      ],
+    },
+  ]
   return (
-    <PopupModal visible={isDisplay} title="新增賽事" onCancel={onCancel}>
+    <PopupModal visible={isDisplay} title="新增推廣連結" onCancel={onCancel}>
       <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <FormField label="請選擇隊伍(主)" name="mainTeam" required>
-          <Input />
+        <FormField label="帳號名稱" name="mainTeam" required>
+          <h3 style={{ margin: 0 }}>aaahcd (總代理)</h3>
         </FormField>
-        <FormField label="請選擇隊伍" name="clientTeam" required>
-          <Input />
+        <FormField label="組織層級" required>
+          <CascaderSelector
+            options={options}
+            placeholder="加盟商 / 股東 / 總代"
+          />
         </FormField>
-        <FormField label="國家" name="country" required>
-          <Select placeholder="請選擇" allowClear>
-            <Option value="opt1">巴西</Option>
-            <Option value="opt2">美國</Option>
-          </Select>
-        </FormField>
-        <FormField label="請選擇聯盟" name="league" required>
-          <Input />
-        </FormField>
-        <FormField label="開賽時間" name="startAt" required>
-          <DatePicker style={{ width: '100%' }} />
-        </FormField>
-        <FormField>
-          <p>＊如果上面結果為撤銷則不用選擇</p>
+
+        <FormField
+          label="代理商域名"
+          required
+          extra={
+            <span>
+              限英數、長度5-12{' '}
+              <span style={{ color: 'red' }}>
+                (請注意：新增後不可修改、刪除！)
+              </span>
+            </span>
+          }
+        >
+          <Input.Group compact>
+            <FormField style={{ width: '70%', marginBottom: 0 }}>
+              <Input placeholder="http://google/" />
+            </FormField>
+            <FormField style={{ width: '30%', marginBottom: 0 }}>
+              <Input allowClear defaultValue="fwWefg" />
+            </FormField>
+          </Input.Group>
         </FormField>
 
         <FormField style={{ marginTop: '20px', textAlign: 'center' }}>
