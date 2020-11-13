2191dcb2dc53a31dfc322799fc94bcf87d802628
Summer Chen <mp4bj6ruby@gmail.com>
2020-11-13 14:37:30 +0800

    Announcement: popup

--- src/pages/website/Announcement/containers/PopupCreateForm.tsx
+++ src/pages/website/Announcement/containers/PopupCreateForm.tsx
@@ -1,10 +1,23 @@
 import PopupModal from '@/components/PopupModal'
-import { Button, DatePicker, Input, Select, Space, Form as AntForm } from 'antd'
+import {
+  Button,
+  DatePicker,
+  Input,
+  Select,
+  Space,
+  Form as AntForm,
+  Radio,
+  Row,
+  Col,
+  Tabs,
+  Card,
+} from 'antd'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { toggleCreateModal } from '../reducer'
 import { selectDisplayCreateModal, useTypedSelector } from '../selectors'
 import Form, { FormField } from '@/components/Form'
+import { ContentEditor, FormStaticText } from '@/components'
 const { Option } = Select
 const CreateForm: React.FC = () => {
   const dispatch = useDispatch()
@@ -20,38 +33,54 @@ const CreateForm: React.FC = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
   }
+  const options = [
+    { label: 'aabbb', value: 'aabbb' },
+    { label: 'cccc', value: 'cccc' },
+  ]
   return (
-    <PopupModal visible={isDisplay} title="新增賽事" onCancel={onCancel}>
+    <PopupModal
+      visible={isDisplay}
+      title="新增公告"
+      onCancel={onCancel}
+      width={700}
+    >
       <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <FormField label="請選擇隊伍(主)" name="mainTeam" required>
-          <Input />
-        </FormField>
-        <FormField label="請選擇隊伍" name="clientTeam" required>
-          <Input />
-        </FormField>
-        <FormField label="國家" name="country" required>
-          <Select placeholder="請選擇" allowClear>
-            <Option value="opt1">巴西</Option>
-            <Option value="opt2">美國</Option>
-          </Select>
-        </FormField>
-        <FormField label="請選擇聯盟" name="league" required>
-          <Input />
-        </FormField>
-        <FormField label="開賽時間" name="startAt" required>
-          <DatePicker style={{ width: '100%' }} />
-        </FormField>
+        <Row gutter={16}>
+          <Col span={12}>
+            <FormField label="收件人">
+              <Radio.Group defaultValue="opt1">
+                <Radio value="opt1">會員</Radio>
+                <Radio value="opt2">代理</Radio>
+              </Radio.Group>
+            </FormField>
+          </Col>
+          <Col span={12}>
+            <FormField label="帳號">
+              <Select
+                mode="multiple"
+                allowClear
+                options={options}
+                defaultValue={['aabbb']}
+              />
+            </FormField>
+          </Col>
+        </Row>
         <FormField>
-          <p>＊如果上面結果為撤銷則不用選擇</p>
+          <FormStaticText>發送對象： aabbb</FormStaticText>
         </FormField>
+        <Tabs defaultActiveKey="cn" type="card" size="small">
+          <Tabs.TabPane tab="簡中" key="cn">
+            <FormField>
+              <ContentEditor />
+            </FormField>
+          </Tabs.TabPane>
+        </Tabs>
 
         <FormField style={{ marginTop: '20px', textAlign: 'center' }}>
           <Space size="large">
+            <Button htmlType="reset">重置</Button>
             <Button type="primary" htmlType="submit">
               送出
-            </Button>
-            <Button onClick={onCancel} htmlType="reset">
-              取消
             </Button>
           </Space>
         </FormField>
