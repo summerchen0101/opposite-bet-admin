03dbb5f77a426758ee577997ee96b9a8841a811b
Summer Chen <mp4bj6ruby@gmail.com>
2020-11-18 16:00:05 +0800

    AdminAccount: search components updated

--- src/pages/admin/AdminAccount/index.tsx
+++ src/pages/admin/AdminAccount/index.tsx
@@ -1,16 +1,11 @@
 import Dashboard from '@/components/Dashboard'
-import PageSearchBar from '@/components/PageSearchBar'
 import { useReducerInjector } from '@/utils/hooks'
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import PageHeader from './components/PageHeader'
-import AccountSearch from './containers/AccountSearch'
-import IpSearch from './containers/IpSearch'
 import PopupCreateForm from './containers/PopupCreateForm'
-import RoleSelector from './containers/RoleSelector'
-import StatusSelector from './containers/StatusSelector'
-import TableData from './containers/TableData'
 import SearchForm from './containers/SearchForm'
+import TableData from './containers/TableData'
 import reducer, { fetchAdminList, initSearchState, moduleName } from './reducer'
 
 const Manager: React.FC = () => {
@@ -23,12 +18,6 @@ const Manager: React.FC = () => {
   return (
     <Dashboard>
       <PageHeader />
-      {/* <PageSearchBar>
-        <AccountSearch />
-        <RoleSelector />
-        <StatusSelector />
-        <IpSearch />
-      </PageSearchBar> */}
       <SearchForm />
       <div style={{ marginTop: '15px' }}></div>
       <TableData />
