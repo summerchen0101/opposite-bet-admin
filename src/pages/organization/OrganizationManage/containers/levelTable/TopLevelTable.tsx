9bb457929e74442b7cd8a447f3723b688e09ad77
Summer Chen <mp4bj6ruby@gmail.com>
2020-12-07 11:46:45 +0800

    OrganizationManage: TopLevelTable updated

--- src/pages/organization/OrganizationManage/containers/levelTable/TopLevelTable.tsx
+++ src/pages/organization/OrganizationManage/containers/levelTable/TopLevelTable.tsx
@@ -1,5 +1,4 @@
-import IconLink from '@/components/IconLink'
-import TableSets from '@/components/TableSets'
+import { TableSets, Text, IconLink } from '@/components'
 import { ColumnsGenerator } from '@/types'
 import { toDateTime } from '@/utils/transfer'
 import { EditOutlined, LockOutlined } from '@ant-design/icons'
@@ -14,7 +13,8 @@ interface TableItem {
   subAccCount: number
   status: boolean
   whiteIPCount: number
-  points: number
+  pendingPoints: number
+  bettingPoints: number
   memberCount: number
   memberPoints: number
   failTimes: number
@@ -28,39 +28,73 @@ interface TableItem {
 const createColumns: ColumnsGenerator<TableItem> = (data) => {
   return [
     {
-      title: '廠商',
-      render: (_, row) => row.account,
-      width: 100,
+      title: '階層資訊',
+      children: [
+        {
+          title: '廠商',
+          render: (_, row) => row.account,
+          width: 100,
+        },
+        {
+          title: '下層',
+          render: (_, row) => {
+            const { setCurrentTable } = useTablePicker()
+            return (
+              <a className="link" onClick={(e) => setCurrentTable('main')}>
+                {row.childCount}
+              </a>
+            )
+          },
+          width: 100,
+        },
+        { title: '子帳號', render: (_, row) => row.subAccCount, width: 100 },
+      ],
     },
     {
-      title: '下層',
-      render: (_, row) => {
-        const { setCurrentTable } = useTablePicker()
-        return (
-          <a className="link" onClick={(e) => setCurrentTable('main')}>
-            {row.childCount}
-          </a>
-        )
-      },
-      width: 100,
+      title: '會員資訊',
+      children: [
+        {
+          title: '未結注額',
+          render: (_, row) => row.pendingPoints,
+          width: 100,
+        },
+        { title: '已下注', render: (_, row) => row.bettingPoints, width: 100 },
+        { title: '人數', render: (_, row) => row.memberCount, width: 100 },
+        { title: '餘額', render: (_, row) => row.memberPoints, width: 100 },
+      ],
     },
-    { title: '子帳號', render: (_, row) => row.subAccCount, width: 100 },
-    { title: '啟/停用', render: (_, row) => row.status, width: 100 },
-    { title: '白名單', render: (_, row) => row.whiteIPCount, width: 100 },
-    { title: '廠商餘額', render: (_, row) => row.points, width: 100 },
-    { title: '會員人數', render: (_, row) => row.memberCount, width: 100 },
-    { title: '會員餘額', render: (_, row) => row.memberPoints, width: 100 },
-    { title: '失敗次數', render: (_, row) => row.failTimes, width: 100 },
     {
-      title: '註冊/最後登入',
-      render: (_, row) => (
-        <>
-          註冊：{toDateTime(row.registerAt)} <br />
-          登入：{toDateTime(row.loginAt)} <br />
-          登入IP：{row.loginIP}
-        </>
-      ),
-      width: 230,
+      title: '狀態',
+      children: [
+        {
+          title: '啟/停用',
+          render: (_, row) =>
+            row.status ? (
+              <Text color="success">啟用</Text>
+            ) : (
+              <Text color="danger">停用</Text>
+            ),
+          width: 100,
+        },
+        { title: '白名單', render: (_, row) => row.whiteIPCount, width: 100 },
+      ],
+    },
+    {
+      title: '登入資訊',
+      children: [
+        { title: '失敗次數', render: (_, row) => row.failTimes, width: 100 },
+        {
+          title: '註冊/最後登入',
+          render: (_, row) => (
+            <>
+              註冊：{toDateTime(row.registerAt)} <br />
+              登入：{toDateTime(row.loginAt)} <br />
+              登入IP：{row.loginIP}
+            </>
+          ),
+          width: 230,
+        },
+      ],
     },
     {
       title: '更新人員/時間',
@@ -92,7 +126,8 @@ const data = [...Array(5)].map((_, i) => ({
   subAccCount: 2,
   status: true,
   whiteIPCount: 3,
-  points: 32200,
+  pendingPoints: 12000,
+  bettingPoints: 4000,
   memberCount: 10,
   memberPoints: 232404,
   failTimes: 2,
@@ -105,7 +140,7 @@ const data = [...Array(5)].map((_, i) => ({
 const TopLevelTable: React.FC = () => {
   return (
     <>
-      <h3>廠商列表</h3>
+      <h5>廠商列表</h5>
       <TableSets<TableItem>
         createColumns={createColumns}
         data={data}
