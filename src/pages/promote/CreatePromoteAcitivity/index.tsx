7587f7997b1058f57cafc39e1da8af3a32abf7ae
Summer Chen <mp4bj6ruby@gmail.com>
2020-12-21 16:51:22 +0800

    promote/CreatePromoteActivity: ui updated

--- src/pages/promote/CreatePromoteAcitivity/index.tsx
+++ src/pages/promote/CreatePromoteAcitivity/index.tsx
@@ -2,7 +2,16 @@ import { ContentEditor, DateRangePicker, ImageUpload } from '@/components'
 import Dashboard from '@/components/Dashboard'
 import Form, { FormField } from '@/components/Form'
 import { statusOpts } from '@/lib/options'
-import { Button, Col, Collapse, Input, Radio, Row, Space } from 'antd'
+import {
+  Button,
+  Col,
+  Collapse,
+  Input,
+  InputNumber,
+  Radio,
+  Row,
+  Space,
+} from 'antd'
 import React from 'react'
 import PageHeader from './components/PageHeader'
 
@@ -12,7 +21,7 @@ const Manager: React.FC = () => {
     <Dashboard>
       <PageHeader />
       <Form>
-        <Row gutter={16}>
+        <Row gutter={32}>
           <Col span={12}>
             <FormField label="活動名稱">
               <Input placeholder="請輸入內容" />
@@ -23,19 +32,25 @@ const Manager: React.FC = () => {
               <Radio.Group options={statusOpts.filter((t) => t.value !== 0)} />
             </FormField>
           </Col>
+          <Col span={12}>
+            <FormField label="活動期間">
+              <Radio.Group defaultValue="opt1">
+                <Radio value="opt1">
+                  <span style={{ marginRight: '5px' }}>無限期</span>
+                </Radio>
+                <Radio value="opt2">
+                  <span style={{ marginRight: '5px' }}>日期區間</span>
+                  <DateRangePicker />
+                </Radio>
+              </Radio.Group>
+            </FormField>
+          </Col>
+          <Col span={12}>
+            <FormField label="活動獎金">
+              <InputNumber style={{ width: '200px' }} placeholder="0" />
+            </FormField>
+          </Col>
         </Row>
-
-        <FormField label="活動期間">
-          <Radio.Group defaultValue="opt1">
-            <Radio value="opt1">
-              <span style={{ marginRight: '5px' }}>無限期</span>
-            </Radio>
-            <Radio value="opt2">
-              <span style={{ marginRight: '5px' }}>日期區間</span>
-              <DateRangePicker />
-            </Radio>
-          </Radio.Group>
-        </FormField>
 
         <FormField>
           <Collapse defaultActiveKey={['1']}>
